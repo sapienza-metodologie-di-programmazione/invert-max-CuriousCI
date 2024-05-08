53f92ee08b0e90984a52640deddbbadc69b1141e
Ionut Cicio <ionutcicio.dev@gmail.com>
2024-05-08 19:20:06 +0200

    Trying to solve the exercise...

--- src/main/java/metodologie/App.java
+++ src/main/java/metodologie/App.java
@@ -1,14 +1,30 @@
 package metodologie;
 
+import java.util.List;
+
+class Inverter<T> implements Invert<T> {
+    @Override
+    public List<T> invert(List<T> list) {
+        return list;
+    }
+}
+
+class Maximizer<T extends Comparable<T>> implements Max<T> {
+    @Override
+    public T max(List<T> list) {
+        return null;
+    }
+}
+
 public class App {
     public static void main(String[] args) {
     }
 
     public static <T> Invert<T> inverter() {
-        return null;
+        return new Inverter<T>();
     }
 
-    public static <T> Max<T> maximizer() {
-        return null;
+    public static <T extends Comparable<T>> Max<T> maximizer() {
+        return new Maximizer<T>();
     }
 }
